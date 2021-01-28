b114cf05aca521a04f2d6568ca7816e1b2663d98
selfstudymaster <webselfmaster@gmail.com>
2021-01-28 09:12:40 +0900

    エラー解消したけどレスポンスが200以外でfalse

--- lib/main.dart
+++ lib/main.dart
@@ -8,100 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<Album> fetchAlbum() async {
-  final response = await http.get('https://connpass.com/api/v1/event/');
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-// albumクラスを定義
-class Album {
-  final int results_returned;
-  final int event_id;
-  final int userId; // メンバ変数
-  final int id; // メンバ変数
-  final String title; // メンバ変数
-
-  // albumインスタンス
-  Album(
-      {this.results_returned, this.event_id, this.userId, this.id, this.title});
-
-  // factoryコンストラクタを実装
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      results_returned: json['results_returned'],
-      event_id: json['event_id'],
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    futureAlbum = fetchAlbum();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-/*
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-Future<Album> fetchAlbum() async {
   final response = await http.get('https://connpass.com/api/v1/title/');
 
   if (response.statusCode == 200) {
@@ -177,4 +83,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-*/
